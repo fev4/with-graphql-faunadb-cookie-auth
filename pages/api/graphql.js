168766a54d1af8686a5111b87bcd875020f721e7
Felipe <afvasquezt@gmail.com>
2020-08-18 14:36:27 -0500

    simplify validation

--- pages/api/graphql.js
+++ pages/api/graphql.js
@@ -1,12 +1,62 @@
 import { ApolloServer } from 'apollo-server-micro';
+import cookie from 'cookie';
+import faunadb from 'faunadb';
+import chalk from 'chalk';
+
 import cookieSetter from '../../lib/cookieHelper';
+import { SECRET_COOKIE_NAME, unsetCookieConfig } from '../../lib/cookieConfig';
+import { faunaClient } from '../../lib/graphqlHelper';
 import schema from '../../lib/graphql/schema';
+
+const q = faunadb.query;
+
+const tokenValidation = async ({ req, res }) => {
+  console.log(chalk.gray('\n⚙️  ') + chalk.cyan('context -- tokenValidation'));
+  const { setCookie } = res;
+  let isTokenValid, token;
+  if (req.headers.cookie) {
+    const parsedCookies = cookie.parse(req.headers.cookie);
+    const customCookie = parsedCookies[SECRET_COOKIE_NAME];
+    if (customCookie) {
+      try {
+        isTokenValid = await faunaClient(customCookie).query(
+          q.Call(q.Function('validate_token'), customCookie)
+        );
+        if (isTokenValid === true) {
+          token = customCookie;
+          console.log(
+            '   context -- tokenValidation --',
+            chalk.green('token is valid!')
+          );
+          /* Don't reset the cookie with `setCookie`, as it would restart its maxAge time.
+          Setting up the cookie should only be done on login or signup. */
+        } else {
+          token = '';
+          setCookie(SECRET_COOKIE_NAME, '', unsetCookieConfig);
+        }
+      } catch (err) {
+        console.log(
+          chalk.red('   context -- tokenValidation failed, clearing cookie'),
+          err.message
+        );
+        token = '';
+        setCookie(SECRET_COOKIE_NAME, '', unsetCookieConfig);
+      }
+    }
+    if (!customCookie) {
+      console.log(chalk.red('   context -- tokenValidation, no cookie found'));
+      token = '';
+    }
+  }
+  return token;
+};
 
 const apolloServer = new ApolloServer({
   schema,
-  context(ctx) {
-    return ctx;
-  },
+  context: async (ctx) => ({
+    token: await tokenValidation(ctx),
+    ...ctx,
+  }),
   introspection: !(process.env.NODE_ENV === 'production'),
   playground: !(process.env.NODE_ENV === 'production'),
 });
