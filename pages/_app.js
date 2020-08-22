07cc77b5deaabd2a8daba4e611ca659f95ac86dd
Felipe <afvasquezt@gmail.com>
2020-08-22 06:16:57 -0500

    add reactquery devtools

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { ReactQueryDevtools } from 'react-query-devtools';
 import { UserContextProvider } from '../components/UserContext';
 
 function MyApp({ Component, pageProps }) {
   return (
     <UserContextProvider>
       <Component {...pageProps} />
+      <ReactQueryDevtools initialIsOpen />
     </UserContextProvider>
   );
 }
